c4a8e3e5197bad31ba73f610642d722aac6fbd2e
David Wengier <git@wengier.com>
2020-10-14 11:29:27 +1100

    Initial Commit

---
+++ ConsoleApp/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+
+namespace ConsoleApp
+{
+    enum Simple
+    {
+        First,
+        Second,
+
+    }
+
+    enum Complex
+    {
+        First = 3,
+        Second = 14,
+        Third = 14,
+        Fourth = 8,
+        Fifth = 9
+    }
+
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            DoSomethingSimple(Simple.Second);
+            DoSomethingComplex(Complex.Fourth);
+
+            DoSomethingComplex((Complex)5);
+        }
+
+        static void DoSomethingSimple(Simple simple)
+        {
+            EnumValidation.EnumValidator.Validate(simple);
+
+            Console.WriteLine("Doing someting complex with " + simple);
+        }
+
+        static void DoSomethingComplex(Complex complex)
+        {
+            EnumValidation.EnumValidator.Validate(complex);
+
+            Console.WriteLine("Doing someting complex with " + complex);
+        }
+    }
+}
